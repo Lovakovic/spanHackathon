6784ee07fffea4f5c35b81aa2b3dc5aaf580dd17
Lovakovic <lovkovdario@gmail.com>
2023-03-18 23:14:20 +0100

    Create a more detailed report in case of pulses or malware reports found

--- node_server/backend/controllers/assessment.controller.js
+++ node_server/backend/controllers/assessment.controller.js
@@ -1,10 +1,11 @@
 const { pool } = require('../app');
 const alienvault = require('../services/alienvault');
+const safebrowsing = require('../services/safebrowsing');
 
-const assessUrl = async (req, res) => {
+const getAssessment = async (req, res) => {
     try {
         const url = req.params.url;
-        const report = await alienvault.assessUrl(url);
+        let report = await alienvault.assess(url);
 
         // Insert short report
         let conn = await pool.getConnection();
@@ -12,13 +13,21 @@ const assessUrl = async (req, res) => {
             '(?, ?)', [report.pulseCount, report.malwareCount]);
         conn.release();
 
+        // Get a more detailed report
+        if(report.pulseCount || report.malwareCount) {
+            report = {
+                ...report,
+                matches: await safebrowsing.assess(url).matches
+            };
+        }
+
         return res.status(200).json(report);
     } catch (err) {
         console.log(err);
         return res.status(500).send();
     }
-}
+};
 
 module.exports = {
-    assessUrl
+    assessUrl: getAssessment
 }
