3457593863e5b852e2b99ae0b70b5df0607925af
Lovakovic <lovkovdario@gmail.com>
2023-03-18 21:52:57 +0100

    Replace `virusTotal` api with `safeBrowsing` api

--- node_server/backend/config.js
+++ node_server/backend/config.js
@@ -8,7 +8,8 @@ module.exports = {
         database: 'hackathon',
         debug: false
     },
-    baseUrl: 'https://otx.alienvault.com',
+    baseAlienVaultUrl: 'https://otx.alienvault.com',
     alienVaultApiKey: 'd5ff7a0e3d91b331cdb49bd38e1f65ab57a4f40a03b32409fcf2d95af9f2b22a',
-    virusTotalApiKey: `ec411e9e674b9b2f656f8665f61490c05b0e6bac0c7d68f560d66a9f80353526`
+    safeBrowsingBaseUrl: 'https://www.virustotal.com',
+    safeBrowsingApiKey: `AIzaSyC2sV9tWdvMWJyO5p-GzyGUE4O30U6Nkgo`
 }
