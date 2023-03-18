ea39dd72355cadc557b05c2f5210cee81e93c7c3
Lovakovic <lovkovdario@gmail.com>
2023-03-18 19:05:04 +0100

    Change `baseUrl` to actual base url

--- node_server/backend/config.js
+++ node_server/backend/config.js
@@ -8,6 +8,6 @@ module.exports = {
         database: 'hackathon',
         debug: false
     },
-    baseUrl: 'https://otx.alienvault.com/api',
+    baseUrl: 'https://otx.alienvault.com',
     apiKey: 'd5ff7a0e3d91b331cdb49bd38e1f65ab57a4f40a03b32409fcf2d95af9f2b22a'
 }
