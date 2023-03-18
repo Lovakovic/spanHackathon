93b0eb8840d9e515375a498cefdc6a3e972d3cdb
MDBossss <ferrari020203@gmail.com>
2023-03-18 14:39:25 +0100

    login and register working

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import {signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../services/firebase';
 
 const LoginForm = ({handleLoginState}) => {
 
-    const [username,setUsername] = useState();
+    const [email,setEmail] = useState();
     const [password,setPassword] = useState();
+    const [error,setError] = useState(false)
+
+    const navigate = useNavigate()
+
+    const {dispatch} = useContext(AuthContext)
+
+    const handleLogin = () => {
+      if(email && password){
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+  
+          console.log("signed in as: " + user)
+          setError(false);
+          dispatch({type:"LOGIN",payload:user})
+          navigate("/");
+        })
+        .catch((error) => {
+          setError(true);
+          //console.log(error)
+        });
+      }
+    }
 
   return (
-    <div className="login-form">
-        <input type="email" placeholder='email'/>
-        <input type="password" placeholder='password'/>
-        <button>Login</button>
+    <div className="login-form">  
+        {error && <span className='alert'>Invalid login!</span>}
+        <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handleLogin}>Login</button>
         <a onClick={handleLoginState}>Not a user? Register.</a>
     </div>
   )
--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { AuthContext } from '../../context/AuthContext';
+
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../services/firebase';
 
 const RegisterForm = ({handleLoginState}) => {
-    const [username,setUsername] = useState();
+    const [email,setEmail] = useState();
     const [password,setPassword] = useState();
+    const [error,setError] = useState(false);
+
+    const navigate = useNavigate()
+
+    const {dispatch} = useContext(AuthContext)
+
+
+    const handleRegister = () => {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log("user registered")
+          const user = userCredential.user;
+          setError(false)
+          dispatch({type:"LOGIN",payload:user})
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error)
+          setError(true)
+        });
+    }
 
   return (
     <div className="login-form">
-        <input type="email" placeholder='email'/>
-        <input type="password" placeholder='password'/>
-        <button>Register</button>
+        {error && <span className='alert'>Invalid register data!</span>}
+        <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handleRegister}>Register</button>
         <a onClick={handleLoginState}>Already a user? Login</a>
     </div>
   )
