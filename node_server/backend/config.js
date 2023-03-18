8ce1c216151518639129dd379afc3c71453dc2f5
Lovakovic <lovkovdario@gmail.com>
2023-03-18 20:28:32 +0100

    Add `virusTotalApiKey`

--- node_server/backend/config.js
+++ node_server/backend/config.js
@@ -9,5 +9,6 @@ module.exports = {
         debug: false
     },
     baseUrl: 'https://otx.alienvault.com',
-    apiKey: 'd5ff7a0e3d91b331cdb49bd38e1f65ab57a4f40a03b32409fcf2d95af9f2b22a'
+    alienVaultApiKey: 'd5ff7a0e3d91b331cdb49bd38e1f65ab57a4f40a03b32409fcf2d95af9f2b22a',
+    virusTotalApiKey: `ec411e9e674b9b2f656f8665f61490c05b0e6bac0c7d68f560d66a9f80353526`
 }
