830e64428d9b0c4760997cec64f73f762a3ad271
Lovakovic <lovkovdario@gmail.com>
2023-03-18 23:12:25 +0100

    Set `safeBrowsingBaseUrl` to an actual API url

--- node_server/backend/config.js
+++ node_server/backend/config.js
@@ -10,6 +10,6 @@ module.exports = {
     },
     baseAlienVaultUrl: 'https://otx.alienvault.com',
     alienVaultApiKey: 'd5ff7a0e3d91b331cdb49bd38e1f65ab57a4f40a03b32409fcf2d95af9f2b22a',
-    safeBrowsingBaseUrl: 'https://www.virustotal.com',
+    safeBrowsingBaseUrl: 'https://safebrowsing.googleapis.com',
     safeBrowsingApiKey: `AIzaSyC2sV9tWdvMWJyO5p-GzyGUE4O30U6Nkgo`
 }
