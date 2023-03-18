60ea84c4f904fb2def84663052a8cd45f9847561
vjekoboras <vjeko.boras+github@gmail.com>
2023-03-18 16:30:38 +0100

    ljepse
    jos ljepse

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -7,10 +7,12 @@ const LoginForm = ({handleLoginState}) => {
 
   return (
     <div className="login-form">
+        <h2 id="welcome">Welcome back!</h2>
+        <p id="cyberText">Cyber security? Yes</p>
         <input type="email" placeholder='email'/>
         <input type="password" placeholder='password'/>
-        <button>Login</button>
-        <a onClick={handleLoginState}>Not a user? Register.</a>
+        <button class="button">Login</button>
+        <p class="p">Not a user? <a onClick={handleLoginState} id="userLink">Register</a></p>
     </div>
   )
 }
--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -6,10 +6,12 @@ const RegisterForm = ({handleLoginState}) => {
 
   return (
     <div className="login-form">
+        <h2 id="welcome">Welcome!</h2>
+        <p id="cyberText">Cyber security? Yes</p>
         <input type="email" placeholder='email'/>
         <input type="password" placeholder='password'/>
-        <button>Register</button>
-        <a onClick={handleLoginState}>Already a user? Login</a>
+        <button class="button">Register</button>
+        <p class="p">Already a user? <a onClick={handleLoginState} id="userLink">Login</a></p>
     </div>
   )
 }
