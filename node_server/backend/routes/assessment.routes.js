637d1b14113e6ae717c3182e597de8e996f24e6e
Lovakovic <lovkovdario@gmail.com>
2023-03-19 03:47:21 +0100

    Implement and expose `/assessReport/all` endpoint

--- node_server/backend/routes/assessment.routes.js
+++ node_server/backend/routes/assessment.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const assessmentController = require('../controllers/assessment.controller');
 const router = express.Router();
 
-router.get('/:url', assessmentController.getAssessment);
+router.get('/all', assessmentController.getAssessments);
+router.get('/url/:url', assessmentController.getAssessment);
 
 module.exports = router;
